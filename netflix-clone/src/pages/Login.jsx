1869426be01e8d9e67ccb576478141741e4c86d4
petkovpavel45 <petkovpavel45@gmail.com>
2024-05-28 13:45:51 +0300

    adding login and sign up html and css

--- netflix-clone/src/pages/Login.jsx
+++ netflix-clone/src/pages/Login.jsx
@@ -1,10 +1,77 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
-
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [rememberLogin, setRememberLogin] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <div className="w-full h-screen">
+        <img
+          className="hidden sm:block absolute w-full h-full object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/a99688ca-33c3-4099-9baa-07a2e2acb398/42f152b5-6bde-426f-8d7a-090f3a1803eb/BG-en-20240520-popsignuptwoweeks-perspective_alpha_website_small.jpg"
+          alt=""
+        />
+        <div className="bg-black/70 fixed top-0 left-0 w-full h-screen" />
+        <div className="fixed w-full px-4 py-24 z-20">
+          <div className="max-w-[450px] h-[600px] mx-auto bg-black/80 rounded-lg">
+            <div className="max-w-[320px] mx-auto py-16">
+              <h1 className="text-3xl font-nsans-bold">Login</h1>
+              <form
+                onSubmit={handleFormSubmit}
+                className="w-full flex flex-col py-4"
+              >
+                <input
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  placeholder="email"
+                  autoComplete="email"
+                  className="p-3 my-2 bg-gray-700 rounded"
+                />
+                <input
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  placeholder="password"
+                  autoComplete="current-password"
+                  className="p-3 my-2 bg-gray-700 rounded"
+                />
+
+                <button className="bg-red-600 py-3 my-6 rounded font-nsans-bold">
+                  Login
+                </button>
+
+                <div className="flex justify-between items-center text-gray-600">
+                  <p>
+                    <input
+                      type="checkbox"
+                      className="mr-2"
+                      checked={rememberLogin}
+                      onChange={() => setRememberLogin(!rememberLogin)}
+                    />{" "}
+                    Remember me
+                  </p>
+                  <p>Need Help?</p>
+                </div>
+                <p className="my-4">
+                  <span className="text-gray-600 mr-2">
+                    New to Netflix?
+                  </span>
+                  <Link to="/signup">Sign Up</Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
